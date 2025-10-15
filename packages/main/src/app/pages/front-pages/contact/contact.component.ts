f4d2c5ca8a2d37ef60c1831e2678e8a7b575d298
jayakumartouchstonelc <jayakumar.k@touchstonelc.com>
2025-10-15 10:54:28 +0530

    till home page settings update

--- packages/main/src/app/pages/front-pages/contact/contact.component.ts
+++ packages/main/src/app/pages/front-pages/contact/contact.component.ts
@@ -3,7 +3,7 @@ import { IconModule } from 'src/app/icon/icon.module';
 import { MaterialModule } from 'src/app/material.module';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
-
+import { baseUrlPathslash } from '../../../config';
 @Component({
   selector: 'app-contact',
   imports: [MaterialModule,IconModule,FooterComponent,CommonModule,],
@@ -11,17 +11,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent implements OnInit{
-    baseUrlPathslash = ''; // You can set this dynamically
+   
   backgroundStyle: any;
+ 
  ngOnInit() {
-    // Example: assign your base path dynamically if needed
-    this.baseUrlPathslash = window.location.origin; 
     this.setBackground();
-  }
+ }
 
   setBackground() {
     this.backgroundStyle = {
-      'background-image': `url('${this.baseUrlPathslash}/assets/images/backgrounds/profilebg.png')`,
+      'background-image': `url('${baseUrlPathslash}/assets/images/backgrounds/profilebg.png')`,
       'background-size': 'cover',
       'background-position': 'center',
       'background-repeat': 'no-repeat'
