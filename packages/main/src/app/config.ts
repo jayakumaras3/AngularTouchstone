e50a652b8f4dafe1e17afa7c744ccd5072ffa661
jayakumartouchstonelc <jayakumar.k@touchstonelc.com>
2025-11-26 13:38:39 +0530

    updated price page

--- packages/main/src/app/config.ts
+++ packages/main/src/app/config.ts
@@ -142,16 +142,16 @@ export const logoUrl = '/ang/';
 
 
 // PHP service URLs
-export const apiUrl = 'http://172.16.0.173/landing';
+/*export const apiUrl = 'http://172.16.0.173/landing';
   
   export const forgotUrl = 'http://172.16.0.173/landing';
   export const contactApiUrl = 'http://172.16.0.173/landing';
-/*
 
+*/
    // Dochek
   export const apiUrl = 'https://dochek.com/landing';
   
   export const forgotUrl = 'https://dochek.com/landing';
   export const contactApiUrl = 'https://dochek.com/landing'
-*/
+
 
