92c5929d65f4c3abec88526456480261eb434767
jayakumartouchstonelc <jayakumar.k@touchstonelc.com>
2026-07-02 14:06:39 +0530

    Switch app envs to dochek.com
    Moves the Angular app to the dochek.com endpoints, updates the environment toggles for local/staging/prod targets, and removes the reset-link expiry note from the password reset screens. Also refreshes the blog image asset.

--- packages/main/src/environments/environment.prod.ts
+++ packages/main/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 // Production Server - 172.16.0.173/DOCHEK
-/**/export const environment = {
+/*export const environment = {
   production: true,
   apiUrl: 'http://172.16.0.173/DOCHEK/landing',
   forgotUrl: 'http://172.16.0.173/DOCHEK',
   contactApiUrl: 'http://172.16.0.173/DOCHEK/landing'
-};
+};*/
 
 /**/
 // Alternative Staging
@@ -15,10 +15,10 @@
   contactApiUrl: 'https://staging.dochek.com/DOCHEK/landing'
 };
 */
-/*
+/**/
 export const environment = {
   production: true,
   apiUrl: 'https://dochek.com/landing',
   forgotUrl: 'https://dochek.com/landing',
   contactApiUrl: 'https://dochek.com/landing'
-}*/
+}
